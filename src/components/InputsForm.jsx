5914270438cbdcd437260d5f82610853dc8c338c
Gastón Aragón <gaston.ara.o@gmail.com>
2023-05-26 20:48:09 -0500

    correccion de alineacion de items

--- src/components/InputsForm.jsx
+++ src/components/InputsForm.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/inputs-form.module.css'
 
 const InputsForm = (props) => {
   return (
-    <div>
+    <div className={styles.form_container}>
         <form action="" className={styles.form}>
             <input className={styles.form_input} type="text" name='asunto' placeholder="Asunto" required/>
             <input className={styles.form_input} type="number" name='monto' placeholder="Monto" required/>
