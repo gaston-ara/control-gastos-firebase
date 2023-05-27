b5354bbf8c10994869bff931a5e6b5f1e7a1d07b
Gastón Aragón <gaston.ara.o@gmail.com>
2023-05-26 20:21:20 -0500

    correccion de alineacion de items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,9 +5,11 @@ import styles from '../styles/item.module.css'
 const Item = (props) => {
   return (
     <div className={styles.item} data-index={props.i} id={props.id}>
+      <div className={styles.item_info}>
+        <div className={styles.item_date}>{props.time}</div>
         <h3>{props.asunto}</h3>
         <p>${props.monto}</p>
-        <span>{props.time}</span>
+      </div>
         <button className={styles.btn_item} type='button' onClick={(e)=>props.delete(e)}><IconDelete/></button>
     </div>
   )
